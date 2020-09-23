e5c887d190093144c9e286ae5559a68a971ce63f
Yiqing Liu <yiqing@strongsalt.com>
2020-09-23 16:01:58 -0700

    argon2 use defaultThreads=1

--- kdf/argon2/argon2.go
+++ kdf/argon2/argon2.go
@@ -15,7 +15,7 @@ const (
 	versionTypeName = "Argon2Version"
 	defaultIter     = int32(1)
 	defaultMemory   = int32(64 * 1024)
-	defaultThreads  = int32(4)
+	defaultThreads  = int32(1) // DO NOT CHANGE libsodium only supports 1 thread
 	defaultSaltLen  = 16
 )
 
@@ -56,7 +56,6 @@ type Argon2 struct {
 	salt    []byte
 	iter    int32
 	memory  int32
-	threads int32
 }
 
 func (_ *Argon2) New() (KdfBase, error) {
@@ -73,16 +72,15 @@ func (_ *Argon2) New() (KdfBase, error) {
 		salt:    salt,
 		iter:    defaultIter,
 		memory:  defaultMemory,
-		threads: defaultThreads,
 	}, nil
 }
 
 // The serialization/deserialization format is as follows:
 
 // Version 1:
-// -----------------------------------------------------------------------------------------------------------------------------------------------
-// | version(4 bytes) | saltLen(4 bytes) | salt | iterLen(4 bytes) | iter(4 bytes) | memoryLen(4 bytes) | memory | threadsLen(4 bytes) | threads |
-// -----------------------------------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------
+// | version(4 bytes) | saltLen(4 bytes) | salt | iterLen(4 bytes) | iter(4 bytes) | memoryLen(4 bytes) | memory |
+// ---------------------------------------------------------------------------------------------------------------
 
 func (_ *Argon2) Deserialize(data []byte) (KdfBase, error) {
 	if len(data) < version.VersionSerialSize {
@@ -130,14 +128,6 @@ func (_ *Argon2) Deserialize(data []byte) (KdfBase, error) {
 			return nil, err
 		}
 		result.memory = memory
-		// threads
-		_ = buf.Next(4)
-		var threads int32
-		err = binary.Read(buf, binary.BigEndian, &threads)
-		if err != nil {
-			return nil, err
-		}
-		result.threads = threads
 	}
 	return result, nil
 }
@@ -173,20 +163,11 @@ func (k *Argon2) Serialize() ([]byte, error) {
 		if err != nil {
 			return nil, err
 		}
-		// threads
-		err = binary.Write(buf, binary.BigEndian, int32(4))
-		if err != nil {
-			return nil, err
-		}
-		err = binary.Write(buf, binary.BigEndian, k.threads)
-		if err != nil {
-			return nil, err
-		}
 		return buf.Bytes(), nil
 	}
 	return nil, nil
 }
 
 func (k *Argon2) GenerateKey(password []byte, keyLen int) ([]byte, error) {
-	return argon2.IDKey(password, k.salt, uint32(k.iter), uint32(k.memory), uint8(k.threads), uint32(keyLen)), nil
+	return argon2.IDKey(password, k.salt, uint32(k.iter), uint32(k.memory), uint8(defaultThreads), uint32(keyLen)), nil
 }
